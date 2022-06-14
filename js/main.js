71b9688fd6f3923eb5f1b174f1894fafcf8b37e3
JoSujeong <106740065+suzzeong@users.noreply.github.com>
2022-06-14 18:53:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -21,6 +21,48 @@ $(function(){
     dots: true,
   });
 
+  $('.product_slider').slick({
+    arrows:false,
+    slidesToShow: 5,
+    dots: true
+  });
+
+  $('.product_list .arrows .left').on('click', function(){
+    $('.product_slider').slick('slickPrev')
+  })
+
+  $('.product_list .arrows .right').on('click', function(){
+    $('.product_slider').slick('slickNext')
+  });
+
+  $('#bgndVideo').YTPlayer();
+
+  $('.utb_movie .left').on('click',function(){
+    $('#bgndVideo').YTPPause();
+  });
+
+  $('.utb_movie .right').on('click',function(){
+    $('#bgndVideo').YTPPlay();
+  });
+
+  $('.customer_tab .tab_menu>li').on('click',function(){
+    var idx=$(this).index(); //0, 1, 2
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.customer_tab .tab_content>div')
+    .eq(idx).addClass('on')
+    .siblings()
+    .removeClass('on');
+  });
+
+  $('#flink').on('change', function(){
+    var lik = $(this).val();
+    if(lik) window.open(lik)
+  });
+
+  $('.popup button').on('click',function(){
+    $(this).parent().hide();
+  })
+
 })
 
 
